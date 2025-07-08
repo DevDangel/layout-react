e53a835a29a87742c7b036da5f0430d6782614c2
Angel H <angeldavidh18@gmail.com>
2025-07-08 17:26:56 -0500

    buena onda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,23 @@ const Header = () => {
     return (
         <header className='header-main'>
             <div className="nav-header">
-            <div className="title-nav">
-                <h1>Bethany</h1>
-            </div>
-            <nav>
-                <ul className="nav-links">
-                    <li><a href="#home">HOME</a></li>
-                    <li><a href="#about">ABOUT</a></li>
-                    <li><a href="#services">SERVICES</a></li>
-                    <li><a href="#portfolio">PORTFOLIO</a></li>
-                    <li><a href="#team">TEAM</a></li>
-                    <li><a href="#contact">CONTACT</a></li>
-                </ul>
-                <a href="#" className="btn-nav">
-                <span>Get Started</span>
-                </a>
-            </nav>
+                <div className="title-nav">
+                    <h1>Angel Inc</h1>
+                </div>
+                <nav>
+                    <ul className="nav-links">
+                        <li><a href="#home">HOME</a></li>
+                        <li><a href="#about">ABOUT</a></li>
+                        <li><a href="#services">SERVICES</a></li>
+                        <li><a href="#portfolio">PORTFOLIO</a></li>
+                        <li><a href="#team">TEAM</a></li>
+                        <li><a href="#contact">CONTACT</a></li>
+                        <li><input type="text" placeholder='Buscar...'/></li>
+                    </ul>
+                    <a href="#" className="btn-nav">
+                    <span>Get Started</span>
+                    </a>
+                </nav>
             </div>
             <div className="img-header">
                 <h1>Online Presence with Bethany</h1>
